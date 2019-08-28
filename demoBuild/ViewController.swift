1bdb902d52fb562ba397456045f6d52880182465
Josh Gimenes <josh.gimenes1@gmail.com>
2019-08-28 10:46:22 -0600

    First COmmit

---
+++ demoBuild/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  demoBuild
+//
+//  Created by Josh GImenes on 8/26/19.
+//  Copyright © 2019 Josh GImenes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    // MARK: Properties
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var mealNameLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
